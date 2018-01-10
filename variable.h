9d7fd7644ead167ac2eefe0aa0c5df3b8bc139e5
Victor Lecomte <csd3449@csd.uoc.gr>
2018-01-10 19:12:27 +0200

    Added overload for comparisson '==' operator

--- variable.h
+++ variable.h
@@ -1,13 +1,26 @@
 #include <string>
 
-class Variable{
+class Variable{	//TODO check class templates
 private:
 	std::string word;
 	double number;
 	bool boolean;
 public:
-	Variable(std::string word) : word(word){}
-	Variable(double number) : number(number){}
-	Variable(bool boolean) : boolean(boolean){}
+	Variable(std::string word) : word(word){};
+	Variable(double number) : number(number){};
+	Variable(int number) : number(number){};
+	Variable(bool boolean) : boolean(boolean){};
+
+	friend std::ostream& operator<< (std::ostream& stream, const Variable& var) {
+		std::string s;
+//		s = std::to_string(static_cast<long long>(number));
+		return stream << s;
+    }
+
+	inline bool operator==(const Variable& rhs) {
+		return (number == rhs.number && number != NULL) || (boolean == rhs.boolean && boolean);
+	}
+
 };
+
 
