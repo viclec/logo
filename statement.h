4a1f2e34aa2e65a9867b1831274366abbf1bd05e
Victor Lecomte <csd3449@csd.uoc.gr>
2018-01-10 16:33:59 +0200

    Added methods in order to get repcount and linenumber and to increment repcount

--- statement.h
+++ statement.h
@@ -4,13 +4,26 @@ class Statement{
 private:
 	std::string type;
 	int repcount;
+	int lineNumber;
 	int id;
 	static int count;
 public:
-	Statement(std::string type) : type(type), id(++count){
+	Statement(std::string type, int lineNumber) : type(type), lineNumber(lineNumber), id(++count){
 		type.compare("repeat") == 0 ? repcount = 0 : repcount = -1 ;
 	}
 	~Statement(){}
+	
+	int getRepCount(void){
+		return repcount;
+	}
+	
+	int getLineNumber(void){
+		return lineNumber;
+	}
+
+	void incrementRepCount(void){
+		repcount++;
+	}
 };
 
 int Statement::count = 0;
