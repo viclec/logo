dc424f5847fb7df81674efbf733c3c49e911b7f8
Victor Lecomte <csd3449@csd.uoc.gr>
2018-01-10 09:20:26 +0200

    Added header file with defines for logo syntax

---
+++ defines.h
@@ -0,0 +1,70 @@
+/*VARIABLES*/
+#define MAKE variable;
+#define NUMBER false?-1
+#define WORD false?""
+#define LIST 
+#define ARRAY
+#define BOOLEAN false?0
+#define SENTENCE
+
+/*ARRAY ITEMS*/
+#define ITEM
+#define SETITEM
+
+/*TURTLE MOVEMENT*/
+#define SUM
+#define DIFFERENCE
+#define MINUS
+#define PRODUCT
+#define QUOTIENT
+#define MODULO
+#define ASSIGN
+
+/*IF/ELIF STATEMENTS*/
+#define IF if(
+#define DO ){
+#define ELIF } else IF
+#define END }
+
+/*REPEAT WHILE*/
+#define REPEAT
+#define TIMES
+#define WHILE
+#define REPCOUNT
+
+/*FOREACH*/
+#define FOREACH
+
+/*ELEM SHOW*/
+#define ELEM
+#define SHOW
+
+/*BOOL OPERATORS*/
+#define AND
+#define OR
+#define NOT
+
+/*TURTLE MOVEMENTS AND METHODS*/
+#define FORWARD
+#define BACK
+#define RIGHT
+#define LEFT
+#define CENTER
+#define SETXY
+#define CIRCLE
+#define PRINT_WORD
+
+/*TURTLE MOVEMENT*/
+#define SETPENCOLOR
+#define SETSCREENCOLOR
+#define SETPENSIZE
+#define PENDOWN
+#define PENUP
+
+/*FUNCTIONS*/
+#define TO void
+#define WITH
+#define FSTART {
+#define FEND }
+#define RETURN return
+#define CALL ()
