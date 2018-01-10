b56dda31b1871c774996f34ffc12338500e8e46e
Victor Lecomte <csd3449@csd.uoc.gr>
2018-01-10 12:56:50 +0200

    Added stack "statements" for statements repcount mostly

--- defines.h
+++ defines.h
@@ -1,3 +1,12 @@
+#include <iostream>
+#include <vector>
+#include "./statement.h"
+#include <string>
+using namespace ::std;
+
+string statementType;
+vector<Statement> statements;
+
 /*VARIABLES*/
 #define MAKE variable;
 #define NUMBER false?-1
@@ -13,27 +22,29 @@
 
 /*TURTLE MOVEMENT*/
 #define SUM
-#define DIFFERENCE
+//#define DIFFERENCE
 #define MINUS
 #define PRODUCT
 #define QUOTIENT
 #define MODULO
 #define ASSIGN
 
+/*STATEMENT METHODS*/
+#define DO ){ statements.push_back(Statement(statementType));
+#define END } statements.pop_back();
+
 /*IF/ELIF STATEMENTS*/
-#define IF if(
-#define DO ){
-#define ELIF } else IF
-#define END }
+#define IF statementType="if";if(
+#define ELIF } else if(
 
 /*REPEAT WHILE*/
-#define REPEAT
-#define TIMES
+#define REPEAT statementType="repeat"; for(int i = 0; i <
+#define TIMES ; i++
 #define WHILE
 #define REPCOUNT
 
 /*FOREACH*/
-#define FOREACH
+#define FOREACH statementType="repeat";
 
 /*ELEM SHOW*/
 #define ELEM
