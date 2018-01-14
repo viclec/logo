58f3406ea04881b8bd34468ca7d222ad17576c3e
Victor Lecomte <csd3449@csd.uoc.gr>
2018-01-14 19:44:21 +0200

    Added other comparisson operators

--- variable.h
+++ variable.h
@@ -12,19 +12,83 @@ public:
 	Variable(bool boolean) : boolean(boolean){};
 
 	bool operator==(char* string) {
-		return (strcmp(word, string) == 0 && word != NULL);
+		return (strcmp(this->word, string) == 0 && this->word != NULL);
 	}
 
 	bool operator==(int integer) {
-		return (number == integer && number != NULL);
+		return (this->number == integer && this->number != NULL);
 	}
 
 	bool operator==(double dbl) {
-		return (number == dbl && number != NULL);
+		return (this->number == dbl && this->number != NULL);
 	}
 
 	bool operator==(Variable rhs) {
-		return (number == rhs.number && number != NULL);
+		return (this->number == rhs.number && number != NULL);
+	}
+
+	bool operator<=(int number) {
+		return (number <= this->number && number != NULL);
+	}
+
+	bool operator<=(double number) {
+		return (number <= this->number && number != NULL);
+	}
+
+	bool operator<=(bool boolean) {
+		return (boolean <= this->boolean && boolean != NULL);
+	}
+
+	bool operator<=(char* word) {
+		return (strcmp(word, this->word) <= 0 && word != NULL);
+	}
+
+	bool operator>=(int number) {
+		return (number >= this->number && number != NULL);
+	}
+
+	bool operator>=(double number) {
+		return (number >= this->number && number != NULL);
+	}
+
+	bool operator>=(bool boolean) {
+		return (boolean >= this->boolean && boolean != NULL);
+	}
+
+	bool operator>=(char* word) {
+		return (strcmp(word, this->word) >= 0 && word != NULL);
+	}
+
+	bool operator<(int number) {
+		return (number < this->number && number != NULL);
+	}
+
+	bool operator<(double number) {
+		return (number < this->number && number != NULL);
+	}
+
+	bool operator<(bool boolean) {
+		return (boolean < this->boolean && boolean != NULL);
+	}
+
+	bool operator<(char* word) {
+		return (strcmp(word, this->word) < 0 && word != NULL);
+	}
+
+	bool operator>(int number) {
+		return (number > this->number && number != NULL);
+	}
+
+	bool operator>(double number) {
+		return (number > this->number && number != NULL);
+	}
+
+	bool operator>(bool boolean) {
+		return (boolean > this->boolean && boolean != NULL);
+	}
+
+	bool operator>(char* word) {
+		return (strcmp(word, this->word) > 0 && word != NULL);
 	}
 
 	Variable& Variable::operator = (Variable &v) {
@@ -42,16 +106,6 @@ public:
 	Variable& Variable::operator = (double a) {
 		return Variable(a);
 	}
-
-	/*
-	Variable Variable::operator [](int i) const {
-		return list[i];
-	}
-
-	Variable & Variable::operator [](int i) {
-		return list[i];
-	}
-	*/
 
 	double getNumber() {
 		return number;
@@ -83,3 +137,66 @@ bool operator==(char* word, Variable rhs) {
 	return (strcmp(word, rhs.getWord())==0 && word != NULL);
 }
 
+bool operator<=(int number, Variable rhs) {
+	return (number <= rhs.getNumber() && number != NULL);
+}
+
+bool operator<=(double number, Variable rhs) {
+	return (number <= rhs.getNumber() && number != NULL);
+}
+
+bool operator<=(bool boolean, Variable rhs) {
+	return (boolean <= rhs.getBoolean() && boolean != NULL);
+}
+
+bool operator<=(char* word, Variable rhs) {
+	return (strcmp(word, rhs.getWord()) <= 0 && word != NULL);
+}
+
+bool operator>=(int number, Variable rhs) {
+	return (number >= rhs.getNumber() && number != NULL);
+}
+
+bool operator>=(double number, Variable rhs) {
+	return (number >= rhs.getNumber() && number != NULL);
+}
+
+bool operator>=(bool boolean, Variable rhs) {
+	return (boolean >= rhs.getBoolean() && boolean != NULL);
+}
+
+bool operator>=(char* word, Variable rhs) {
+	return (strcmp(word, rhs.getWord()) >= 0 && word != NULL);
+}
+
+bool operator<(int number, Variable rhs) {
+	return (number < rhs.getNumber() && number != NULL);
+}
+
+bool operator<(double number, Variable rhs) {
+	return (number < rhs.getNumber() && number != NULL);
+}
+
+bool operator<(bool boolean, Variable rhs) {
+	return (boolean < rhs.getBoolean() && boolean != NULL);
+}
+
+bool operator<(char* word, Variable rhs) {
+	return (strcmp(word, rhs.getWord()) < 0 && word != NULL);
+}
+
+bool operator>(int number, Variable rhs) {
+	return (number > rhs.getNumber() && number != NULL);
+}
+
+bool operator>(double number, Variable rhs) {
+	return (number > rhs.getNumber() && number != NULL);
+}
+
+bool operator>(bool boolean, Variable rhs) {
+	return (boolean > rhs.getBoolean() && boolean != NULL);
+}
+
+bool operator>(char* word, Variable rhs) {
+	return (strcmp(word, rhs.getWord()) > 0 && word != NULL);
+}
