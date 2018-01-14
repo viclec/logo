2511f08863ce7ebbc31870b340f2d6a30038d454
Victor Lecomte <csd3449@csd.uoc.gr>
2018-01-14 23:15:49 +0200

    Added overloads

--- list.h
+++ list.h
@@ -1,14 +1,36 @@
 class List {
 private:
 
-	vector<Variable> pA;
+	vector<Variable> items;
 	unsigned int n;
 
 public:
 
-	List(unsigned int n) : n(n) {}
-	~List() {}
+	List() {};
+	List(unsigned int n) : n(n) {};
+	~List() {};
 
+	template<class...T>
+	List& List::operator , (T v...) {
+		items.push_back(v);
+		return *this;
+	}
+
+	List& List::operator , (Variable &v) {
+		items.push_back(v);
+		return *this;
+	}
+
+	List& List::operator = (Variable &v) {
+		items.push_back(v);
+		return *this;
+	}
+
+	Variable& operator[](unsigned int i) { return items.at(i); }
+	const Variable& operator[](unsigned int i)const { return items.at(i); }
+
+	Variable& operator()(unsigned int i) { return items.at(i); }
+	const Variable& operator()(unsigned int i)const { return items.at(i); }
 
 	unsigned int size() { return n; }
 	unsigned int size()const { return n; }
